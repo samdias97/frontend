7c7e707c46de7daaf1980912e461b1b92785a2d4
Samuel Dias <samuel.sdias@hotmail.com>
2020-09-06 20:21:54 -0300

    first commit

---
+++ public/script.js
@@ -0,0 +1,6 @@
+/**
+ * Babel: Converter (transpilar) código React para um código que o browser entenda.
+ * Webpack: Para cada tipo de arquivo (.js, .css, .png) eu vou converter o código de uma maneira diferente.
+ * 
+ * Loaders: babel-loader, css-loader, image-loader
+ */
